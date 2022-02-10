a906d2508bbbf3e7f0979926c213c448c079a566
ldex <laurent.duveau+git@gmail.com>
2022-02-10 14:20:58 -0500

    New Product Form

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,20 +4,23 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
+import { ProductInsertComponent } from './product-insert/product-insert.component';
 
 
 @NgModule({
   declarations: [
     ProductDetailComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductInsertComponent
   ],
   exports: [ProductListComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialModule
   ]
 })
